1a4d7c3a60a51594d20a29b96c570f5a755169f7
培然 李 <317558138@qq.com>
2020-05-07 14:43:37 +0800

    完成了函数Apply_move,Is_Over,评估函数和等级判断函数

--- chess.cpp
+++ chess.cpp
@@ -15,12 +15,47 @@ function:
 	根据棋盘this->Board，判断当前执子方是否无棋可走
 */
 
+int Chess::Rank_Judgement(char a, char b)
+{
+	//新位置为空
+	if (a == blank)
+		return UNDER;
+	if (b == blank)
+		return ABOVE;
+	if (a >= 'a' && a <= 'z')
+		a = a - 'a' + 'A';
+	if (b >= 'a' && b <= 'z')
+		b = b - 'a' + 'A';
+	if (a == 'Z' || b == 'Z'||a == b)//特殊：炸弹
+		return SAME_RANK;
+	if (a != 'G' && b == 'D' || a == 'D' && b != 'G')//特殊：地雷
+		return SAME_RANK;
+	if (a == 'G' && b == 'D')//特殊：工兵排雷
+		return ABOVE;
+	if (a == 'D' && b == 'G')//特殊：工兵排雷
+		return UNDER;
+	//以下为等级顺序
+	int a_order;
+	int b_order;
+	for (int i = 0; i < 10; i++) {
+		if (a == RANK[i])
+			a_order = i;
+		if (b == RANK[i])
+			b_order = i;
+	}
+	if (a_order < b_order)
+		return ABOVE;
+	else
+		return UNDER;
+}
+
 bool Chess::Is_Over(const int& Role)
 {
 	/* NEED CODE */
-	//nothing change
-	//just a test
-	return bool();
+	//只考虑了双方大本营内的军旗有没有被吃
+	if((Board[0][1]=='F'||Board[0][3]=='F')&&(Board[12][1]=='f'||Board[12][3]=='f'))
+		return false;
+	return true;
 }
 
 
@@ -44,8 +79,20 @@ function:
 
 int Chess::Evaluate_Chess(const int& Role)
 {
-	/* NEED CODE */
-	return 0;
+	int UPPER = 0;
+	int LOWER = 0;
+	for (int i = 0; i < Chess_H; i++) {
+		for (int j = 0; j < Chess_W; j++) {
+			if (Board[i][j] >= 'A' && Board[i][j] <= 'Z')
+				UPPER++;
+			else
+				LOWER++;
+		}
+	}
+	if (Role == ROLE_LOWER)
+		return LOWER - UPPER;
+	else
+		return UPPER - LOWER;
 }
 
 
@@ -87,7 +134,23 @@ function:
 Chess Chess::Apply_Move(const Movement& V)
 {
 	Chess Next_Board;
-
+	for (int i = 0; i < Chess_H; i++) {
+		for (int j = 0; j < Chess_W; j++) {
+			Next_Board.Board[i][j] == Board[i][j];
+		}
+	}
+	int result = Rank_Judgement(Next_Board.Board[V.To.x][V.To.y], Next_Board.Board[V.From.x][V.From.y]);
+	if (result == SAME_RANK) {
+		Next_Board.Board[V.To.x][V.To.y] = blank;
+		Next_Board.Board[V.From.x][V.From.y] = blank;
+	}
+	else if (result = ABOVE) {
+		Next_Board.Board[V.From.x][V.From.y] = blank;
+	}
+	else {
+		Next_Board.Board[V.To.x][V.To.y] = Next_Board.Board[V.From.x][V.From.y];
+		Next_Board.Board[V.From.x][V.From.y] = blank;
+	}
 	/* NEED CODE */
 
 	return Next_Board;
